d64c5b45169fe0f6abb6f17680176d3b7b149828
Vladislav Yashin <v.yashin.work@gmail.com>
2018-05-21 10:57:54 +0300

    Fix worker test

--- test/integration/workers/issue_chat_auto_close_worker_test.rb
+++ test/integration/workers/issue_chat_auto_close_worker_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../../test_helper', __FILE__)
-# require_relative '../../../app/workers/telegram_group_close_notification_worker.rb'
-# require_relative '../../../app/workers/telegram_group_close_worker.rb'
-# require_relative '../../../app/workers/telegram_group_auto_close_worker.rb'
+require_relative '../../../app/workers/issue_chat_close_notification_worker.rb'
+require_relative '../../../app/workers/issue_chat_close_worker.rb'
+require_relative '../../../app/workers/issue_chat_auto_close_worker.rb'
 require 'minitest/mock'
 require 'minitest/autorun'
 
