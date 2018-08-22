e397c4ae890ae63a99c24f217b83972caaf191b6
Vladislav Yashin <v.yashin.work@gmail.com>
2018-08-22 17:46:20 +0300

    Truncate issue notes in bot messages

--- app/workers/issue_chat_notifications_worker.rb
+++ app/workers/issue_chat_notifications_worker.rb
@@ -3,6 +3,8 @@ class IssueChatNotificationsWorker
   include IssuesHelper
   include CustomFieldsHelper
 
+  MAX_NOTES_LENGTH = 500
+
   ISSUE_NOTIFICATIONS_LOG = Logger.new(Rails.root.join('log/redmine_2chat', 'telegram-issue-notifications.log'))
 
   def perform(im_id, platform_name, journal_id)
@@ -17,7 +19,7 @@ class IssueChatNotificationsWorker
 
     message << "\n#{details_to_strings(journal.visible_details, true).join("\n")}" if journal.details.present?
 
-    message << "\n<pre>#{ActionView::Base.full_sanitizer.sanitize(journal.notes)}</pre>" if journal.notes.present?
+    message << "\n<pre>#{ActionView::Base.full_sanitizer.sanitize(journal.notes).truncate(MAX_NOTES_LENGTH, separator: ' ')}</pre>" if journal.notes.present?
 
     ISSUE_NOTIFICATIONS_LOG.info "MESSAGE: #{message}"
 
