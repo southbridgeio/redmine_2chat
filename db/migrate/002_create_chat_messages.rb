1c4b3f9e3d59d415034bd82151ef021291c31db5
Vladislav Yashin <v.yashin.work@gmail.com>
2018-12-12 10:49:26 +0400

    Adapt for Redmine 4

--- db/migrate/002_create_chat_messages.rb
+++ db/migrate/002_create_chat_messages.rb
@@ -1,4 +1,4 @@
-class CreateChatMessages < ActiveRecord::Migration
+class CreateChatMessages < Rails.version < '5.0' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     create_table :chat_messages do |t|
       t.string :im_id
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,15 +9,6 @@ require 'minitest/reporters'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-DatabaseCleaner.strategy = :transaction
-DatabaseCleaner.clean_with(:truncation)
-
-class Minitest::Spec
-  around do |tests|
-    DatabaseCleaner.cleaning(&tests)
-  end
-end
-
 class ActiveSupport::TestCase
   # Add spec DSL
   extend Minitest::Spec::DSL
