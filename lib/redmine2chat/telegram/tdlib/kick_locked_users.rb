0061d9c5c535200ee42a81db45bc75a1df8cd628
Vladislav Yashin <v.yashin.work@gmail.com>
2019-01-30 09:40:16 +0400

    Fix connection pool issue in IssueChatKickLockedUsersWorker

--- lib/redmine2chat/telegram/tdlib/kick_locked_users.rb
+++ lib/redmine2chat/telegram/tdlib/kick_locked_users.rb
@@ -3,14 +3,18 @@ require_dependency 'telegram_account'
 module Redmine2chat::Telegram::Tdlib
   class KickLockedUsers < RedmineBots::Telegram::Tdlib::Command
     def call
-      promises = IssueChat.where(platform_name: 'telegram').where.not(im_id: nil).map do |group|
+      telegram_accounts = TelegramAccount.where.not(telegram_id: nil)
+                              .joins(:user)
+                              .preload(:user)
+                              .where(users: { status: Principal::STATUS_LOCKED })
+
+      promises = IssueChat.active.where(platform_name: 'telegram').where.not(im_id: nil).map do |group|
         client.get_chat(group.im_id).then do |chat|
           client.get_basic_group_full_info(chat.type.basic_group_id)
-        end.flat.then do |group_info|
-          accounts = TelegramAccount.preload(:user)
-                         .joins(:user)
-                         .where(telegram_id: group_info.members.map(&:user_id))
-                         .where(users: { status: Principal::STATUS_LOCKED })
+        end.flat.rescue { nil }.then do |group_info|
+          next Promises.fulfilled_future(true) if group_info.blank?
+
+          accounts = telegram_accounts.select { |account| account.telegram_id.in?(group_info.members.map(&:user_id)) }
           Promises.zip(*accounts.map { |account| kick_member(group.im_id, account.telegram_id) })
         end.flat
       end
